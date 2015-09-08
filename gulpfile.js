10f3581a09c8273faa5a550380b2e20b53d4a6f2
Navid Nikpour <navid@nikpour.com>
2015-09-08 16:30:27 +0200

    Update: changed binary file to be Javascript instead of Coffeescript in order to be used by build tools like Grunt or Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
+var coffee = require('gulp-coffee');
+var gutil  = require('gulp-util');
+
 var del = require('del');
 
 gulp.task('clean', function(cb) {
@@ -8,12 +11,17 @@ gulp.task('clean', function(cb) {
 
 gulp.task('copy', ['clean'], function() {
   return gulp.src('src/swagged-angular-resources.coffee')
-    .pipe(rename('swagged-angular-resources'))
+    .pipe(coffee({bare: true})).on('error', console.error)
+    .pipe(rename({extname: ''}))
     .pipe(gulp.dest('.bin'));
 });
 
 gulp.task('watch', function() {
   return gulp.watch(['src/*'], ['default']);
 });
+
+gulp.task('test', function(){
+  return gulp.src('*')
+})
 
 gulp.task('default', ['copy']);
